26c545f3e025add8fc0edecb56590053204edd12
Kumimin <kumimin@hamazaki-no-MacBook-Pro.local>
2015-05-06 02:22:52 +0900

    test2を追加

--- GItHubTest5/ViewController.swift
+++ GItHubTest5/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         println("test1")
+        println("test2_ローカルにブランチを作らずそのまま開発")
     }
 
     override func didReceiveMemoryWarning() {
